08a19dd02dcad314c9912486f7aa34a8f9394984
RITU <109500521+Ritu25kumari@users.noreply.github.com>
2023-09-04 16:21:21 +0530

    commit

--- series1_ImagePicker.dart
+++ series1_ImagePicker.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:image_cropper/image_cropper.dart';
 class ImagePickerApp extends StatefulWidget{
   @override
   _ImagePickerAppState createState()=> _ImagePickerAppState();
 }
-class _ImagePickerAppState extends State<ImagePickerApp>{
+class _ImagePickerAppState extends State<ImagePickerApp> {
 
   File? _image;
-  Future getImage(ImageSource source) async{
+
+  Future getImage(ImageSource source) async {
     final image = await ImagePicker().pickImage(source: source);
-    if(image == null)
+    if (image == null)
       return;
 
     final imageTemporary = File(image.path);
@@ -33,76 +33,50 @@ class _ImagePickerAppState extends State<ImagePickerApp>{
             Padding(
               padding: const EdgeInsets.all(30.0),
               child: Container(
-                  height:300,
+                  height: 300,
                   width: 300,
-                  child: _image != null ? Image.file(_image!, width: 250,height: 250,fit: BoxFit.cover,):
-                  Image.network('https://www.technogrips.com/laravel_code/public/blogimages/1585603242.png')),
+                  child: _image != null ? Image.file(
+                    _image!, width: 250, height: 250, fit: BoxFit.cover,) :
+                  Image.network(
+                      'https://www.technogrips.com/laravel_code/public/blogimages/1585603242.png')),
             ),
             SizedBox(width: 10,),
             CustomButton(
                 title: 'Select From Camera',
                 icon: Icons.camera_alt_outlined,
-                onClick:(){getImage(ImageSource.camera);}
+                onClick: () {
+                  getImage(ImageSource.camera);
+                }
             ),
             SizedBox(width: 10,),
             CustomButton(
                 title: 'Select From Gallery',
                 icon: Icons.image_outlined,
-                onClick:() {getImage(ImageSource.gallery);}
+                onClick: () {
+                  getImage(ImageSource.gallery);
+                }
             ),
           ],)
       ),
     );
   }
-  Future cropImage() async {
-    if (_image != null) {
-      CroppedFile? cropped = await ImageCropper().cropImage(
-          sourcePath: _image!.path,
-          aspectRatioPresets:
-          [
-            CropAspectRatioPreset.square,
-            CropAspectRatioPreset.ratio3x2,
-            CropAspectRatioPreset.original,
-            CropAspectRatioPreset.ratio4x3,
-            CropAspectRatioPreset.ratio16x9
-          ],
 
-          uiSettings: [
-            AndroidUiSettings(
-                toolbarTitle: 'Crop',
-                cropGridColor: Colors.black,
-                initAspectRatio: CropAspectRatioPreset.original,
-                lockAspectRatio: false),
-          ]);
+  Widget CustomButton({
+    required String title,
+    required IconData icon,
+    required VoidCallback onClick,
+  }) {
+    return Container(
+      width: 300,
+      child: ElevatedButton(child: Row(children: [
+        Icon(icon),
+        SizedBox(width: 10,),
+        Text(title),
 
-      if (cropped != null) {
-        setState(() {
-          _image = File(cropped.path);
-        });
-      }
-    }
-  }
-  void _clearImage() {
-    setState(() {
-      _image = null;
-    });
+      ],),
+          onPressed: onClick
+      ),
+    );
   }
 }
-Widget CustomButton({
-  required String title,
-  required IconData icon,
-  required VoidCallback onClick,
-}
-    ){
-  return Container(
-    width: 300,
-    child: ElevatedButton (child: Row(children: [
-      Icon(icon),
-      SizedBox(width: 10,),
-      Text(title),
 
-    ],),
-        onPressed: onClick
-    ),
-  );
-}
