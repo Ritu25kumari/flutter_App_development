a2ff95f7c222e7513d730387c2eea2e52fa71d75
RITU <109500521+Ritu25kumari@users.noreply.github.com>
2023-09-14 16:43:21 +0530

    series4_imageGridview.dart

--- series4_imageGridview.dart
+++ series4_imageGridview.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 class PostModel{
   final String imageUrl;
@@ -5,15 +7,91 @@ class PostModel{
   final String Description;
   bool isliked;
   PostModel(this.imageUrl,this.Title,this.Description,this.isliked);
-
 }
-class gridImage extends StatefulWidget {
-
+class GridImage extends StatefulWidget {
+ //  final TextEditingController _title= TextEditingController();
+ // final TextEditingController _Description = TextEditingController();
   @override
-  gridImageState createState() => gridImageState();
+  GridImageState createState() => GridImageState();
 }
 
-class gridImageState extends State<gridImage> {
+class GridImageState extends State<GridImage> {
+  void _showImagePicker(BuildContext context){
+    File? posts;
+Future _getImageCamera() async {
+  final pickedfile = await ImagePicker().pickImage(source: ImageSource.camera);
+  if (pickedfile != null) {
+      setState(() {
+        posts = File(pickedfile!.path);
+      });
+    }
+  }
+Future _getImageGallery() async {
+  final pickedfile = await ImagePicker().pickImage(source: ImageSource.gallery);
+  if (posts != null) {
+    setState(() {
+      posts = File(pickedfile!.path);
+    });
+  }
+}
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return SizedBox(
+          height: 500,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Add Images in PostModel',style: TextStyle(color: Colors.grey),),
+                SizedBox(height: 100,),
+                Column(
+                  children: [
+                     Container(
+                       child: posts != null ? Image.file(
+                            posts!, width: 250, height: 250, fit: BoxFit.cover,) :
+                          Container(height: 100, width: 100, color: Colors.grey,),
+                     ),
+                    ListTile(
+                      leading:Icon(Icons.camera_alt_outlined) ,
+                      onTap:(){
+                        _getImageCamera();
+                      },
+                    ),
+                    ListTile(
+                      leading:Icon(Icons.photo_album_outlined) ,
+                      onTap:(){
+                        _getImageGallery();
+                      },
+                    ),
+
+                  ],
+                ),
+                // TextField()(
+                //   controller: _title,
+                //   decoration: InputDecoration(
+                //     labelText: 'title'
+                //   ),
+                // ),
+                // TextField(
+                //   controller: _Description,
+                //   decoration: InputDecoration(
+                //     labelText: 'Description'
+                //   ),
+                // ),
+                ElevatedButton(
+                  child: const Text('Add',style: TextStyle(color: Colors.black,),),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+
+  }
   final List<PostModel> posts = [
     PostModel('https://cloudfront-us-east-2.images.arcpublishing.com/reuters/F6INOOMSRRL5XOOQDRPZUWPWBA.jpg','Post1101','flutter developer....',false),
     PostModel('https://cloudfront-us-east-2.images.arcpublishing.com/reuters/F6INOOMSRRL5XOOQDRPZUWPWBA.jpg','Post1102','flutter developer....',false),
@@ -30,15 +108,16 @@ class gridImageState extends State<gridImage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.grey,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Image Grid View',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
-            InkWell(child: Icon(Icons.add,color: Colors.black,weight: 20,),
-            onTap: ()=>{
-            },)
-          ],
-        ),
+        title: Text('Image Grid View',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
+        actions: [
+          IconButton(icon: Icon(Icons.add),
+          onPressed: (){
+            _showImagePicker(context);
+          },)
+        ],
+        // InkWell(child: Icon(Icons.add,color: Colors.black,weight: 20,),
+        // onTap: ()=>{ _showImagePicker()
+        // },)
         centerTitle: true,
       ),
       body: Padding(
@@ -80,3 +159,4 @@ class gridImageState extends State<gridImage> {
     );
   }
 }
+
