3bcba7617bc1c4e1b7ab4abf1d167337f99ebc00
RITU <109500521+Ritu25kumari@users.noreply.github.com>
2023-09-22 17:40:30 +0530

    commit

--- main.dart
+++ main.dart
@@ -1,75 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:ritu_trainee/like_button.dart';
-import 'package:ritu_trainee/series1_ImagePicker.dart';
-import 'package:ritu_trainee/series2_DateTimePicker.dart';
-import 'package:ritu_trainee/series3_bmicalculator.dart';
-import 'package:ritu_trainee/series4_imageGridview.dart';
-import 'package:ritu_trainee/series4_imagegrid.dart';
-
+import 'package:ritu_trainee/flutterfirstpro.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Home Page'),
+      home: const MyHomePage1(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MyHomePage1 extends StatefulWidget {
+  const MyHomePage1({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage1> createState() => _MyHomePage1State();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePage1State extends State<MyHomePage1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: Text('Home Page',style: TextStyle(color: Colors.white),),
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Colors.indigoAccent,
+        title: Center(child: Text('Flutter Project',style: TextStyle(fontWeight: FontWeight.bold),)),
       ),
-      body:Center(
-        child: Container(
-          color: Colors.deepPurple[400],
-          height: 300,
-          width: 250,
-          child: Column(
-            children: [
-              ElevatedButton(child: Text('ImagePicker'),
-              onPressed: ()=>Navigator.push(context,MaterialPageRoute(builder: (context)=> ImagePickerApp())),),
-              ElevatedButton(child: Text('Date&Time Picker'),
-              onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>TimeDatePicker())),),
-              ElevatedButton(child: Text('BMI Calculator'),
-              onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>BmiCalculator())),),
-              ElevatedButton(child: Text('Image Grid View'),
-              onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>GridExample())),),
-              ElevatedButton(child: Text('PostModel'),
-              onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>PostImage())),),
-              ElevatedButton( child: Text('new post model'),
-              onPressed: ()=> Navigator.push(context, MaterialPageRoute(builder: (context)=>gridImage())),)
-            ],
-          ),
-        ),
-      )
-       // This trailing comma makes auto-formatting nicer for build methods.
+      body: ElevatedButton(child: Text('First Project'),
+      onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>FirstProject())),),
     );
   }
 }
